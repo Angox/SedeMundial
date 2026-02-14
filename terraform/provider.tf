eb57b93c1987fe8cb618a37de622eb22fddcdb1d
Angox <Angox@users.noreply.github.com>
2026-02-14 12:38:12 +0100

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,16 +4,15 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-    # Añadimos el proveedor de tiempo aquí
     time = {
       source  = "hashicorp/time"
       version = "~> 0.9.0"
     }
   }
   
-  # Backend para guardar el estado
+  # Backend para guardar el estado (MODIFICA EL BUCKET SI ES NECESARIO)
   backend "s3" {
-    bucket         = "mi-empresa-tf-state-lock" # <--- ASEGÚRATE DE QUE ESTE ES TU BUCKET REAL
+    bucket         = "mi-empresa-tf-state-lock" 
     key            = "proyectos/stadiums/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
